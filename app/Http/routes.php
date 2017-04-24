f79cd41bc5d4e0977d901d3d0c2fe46ba48c016e
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-04-24 12:36:07 -0500

    roll dice solution

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,60 +16,61 @@ Route::get('/', function () {
 });
 
 Route::get('/rolldice/{guess}', function($guess) {
-	$random = mt_rand(1, 6);
+    $random = mt_rand(1, 6);
 
-	if(!is_numeric($guess) || ($guess > 6 || $guess < 1)) {
-		$message = "Your guess must be a number between 1 and 6";
-	}
+    if($guess == $random) {
+        $message = "You guessed it!";
+    } else if($guess > $random) {
+        $message = "You guessed too high!";
+    } else {
+        $message = "You guessed too low, yo!";
+    }
 
-	if($guess == $random) {
-		$message = "You guessed it!";
-	} else if($guess > $random) {
-		$message = "You guessed too high!";
-	} else {
-		$message = "You guessed too low, yo!";
-	}
+    if(!is_numeric($guess) || ($guess > 6 || $guess < 1)) {
+        $message = "Your guess must be a number between 1 and 6";
+    }
 
-	$data = [
-		'guess' => $guess,
-		'random' => $random,
-		'message' => $message
-	];
+    
+    $data = [
+        'guess' => $guess,
+        'random' => $random,
+        'message' => $message
+    ];
 
 
-	return view('roll-dice', $data);
+    return view('roll-dice', $data);
 
 
 });
 
 Route::get('/sayhello/{name?}', function($name = "World") {
-	
-	if($name == "Ryan") {
-		return Redirect::to("/");
-	}
+    
+    if($name == "Ryan") {
+        return Redirect::to("/");
+    }
 
-	$data = ['name' => $name];
+    $data = ['name' => $name];
 
     return view('my-first-view', $data);
 });
 
 Route::get('/uppercase/{word}', function($word) {
-	return strtoupper($word);
+    return strtoupper($word);
 });
 
 Route::get('/increment/{number?}', function($number = 0) {
-	if(is_numeric($number)) {
-		return $number + 1;
-	} else {
-		return $number . " is not a number and cannot be incremented.";
-	}
+    if(is_numeric($number)) {
+        return $number + 1;
+    } else {
+        return $number . " is not a number and cannot be incremented.";
+    }
 });
 
 Route::get('/add/{num1}/{num2}', function($num1, $num2) {
-	if(is_numeric($num1) && is_numeric($num2)) {
-		return $num1 + $num2;
-	} else {
-		return "Both parameters must be numeric.";
-	}
+    if(is_numeric($num1) && is_numeric($num2)) {
+        return $num1 + $num2;
+    } else {
+        return "Both parameters must be numeric.";
+    }
 });
 
