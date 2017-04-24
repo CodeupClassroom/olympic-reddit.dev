86e1b8548208dab4bdb96635faa2285029af83c1
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-04-24 11:41:49 -0500

    Add routes exercise solutions

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,23 @@ Route::get('/', function () {
 Route::get('/sayhello/{name?}', function($name = "World") {
     return "Hello, $name!";
 });
+
+Route::get('/uppercase/{word}', function($word) {
+	return strtoupper($word);
+});
+
+Route::get('/increment/{number?}', function($number = 0) {
+	if(is_numeric($number)) {
+		return $number + 1;
+	} else {
+		return $number . " is not a number and cannot be incremented.";
+	}
+});
+
+Route::get('/add/{num1}/{num2}', function($num1, $num2) {
+	if(is_numeric($num1) && is_numeric($num2)) {
+		return $num1 + $num2;
+	} else {
+		return "Both parameters must be numeric.";
+	}
+});
